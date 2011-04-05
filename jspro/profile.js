2700f9238d24395cd39a82e3a5d4b16840fa81d4
auscar <auscar@qq.com>
2011-04-05 23:02:34 +0800

    提交最新的js

--- jspro/profile.js
+++ jspro/profile.js
@@ -8,6 +8,9 @@
 				url:'changeImg.do',
 				data:{'tagId':tagId},
 				cache:false,
+				beforeSend:function(){
+					view.detailText.append( '<img class="loading" src="http://s.jesscool.com/imgpro/wait.gif" />' );
+				},
 				success: function(data){
 					view.renderImg(data);
 				},
@@ -22,162 +25,34 @@
 		init: function(){
 			this.getUIRef();
 			this.bindEvent();
+			this.renderInit();
 		},
 		getUIRef: function(){
 			this.imgNav1_ul = $( '.imgNav1_ul' );
-			this.imgNav1_li = $( '.imgNav1_li' );
+			this.imgNav1_li = $( '#articleType > li' );
+			this.detailText = $( '.detailText' );
 		},
 		bindEvent: function(){
-			this.imgNav_li.click(function(){
-				
+			this.imgNav1_li.click(function(){
+				imgManager.changeImg($(this).find( 'a' ).attr( 'tagId' ));
+				$(this).addClass( 'selected' ).siblings().removeClass( 'selected' );
 			});
 		},
 		renderImg: function(data){
-			
+			this.detailText.html(data);
+		},
+		renderInit: function(){
+			this.imgNav1_li.eq(0).addClass( 'selected' );
 		}
-
-			
 	}
 	
-	view.init();
+	$(function(){
+		
+		view.init();
+	});
 	
 	
-	/*$(function(){
-		//script for menu
-		var $articleType = $("#articleType"),
-			$articleType_li = $articleType.find("li"),
-			$editAndDel = $('<div id="imgTagEditAndDel"><img src="http://s.jesscool.com/imgpro/delete.gif" class="imgTagDelBtm" alt="删除" /><img src="http://s.jesscool.com/imgpro/edit.gif" class="imgTagEditBtm" alt="编辑" /></div>'),
-			$imgTagDelBtm = $editAndDel.find(".imgTagDelBtm"),
-			$imgTagEditBtm = $editAndDel.find(".imgTagEditBtm"),
-			time_editAndDel = 0;
-		$editAndDel.appendTo($("body"));
-		
-		$articleType_li.hover(function(){
-			//追加到当前对象
-			$editAndDel.appendTo($(this)).show().click(function(){ $(this).hide(); });
-		},function(){
-			$editAndDel.hide();
-		});
-		
-		
-		//删除、编辑功能
-			$imgTagDelBtm.click(function(){
-				var com_imgTagDel = confirm("真的要删除该分类吗?"),
-					$thisLi = $(this).parents("li");
-					imgTagId = $thisLi.find(" > a").attr("tagId");
-					
-				if(com_imgTagDel){
-					$editAndDel.appendTo($("body"));
-					$.ajax({
-						type:"post",
-						url:"deleteTag.do",
-						data:{"imgTagId":imgTagId},
-						dataType:"html/text",
-						cache:false,
-						beforeSend:function(){
-							//add waiting imgUrl
-						},
-						success:function(){
-							//清空数组中当前tag的内容
-							for(var li in menuShowAry){
-								if(menuShowAry[li] == $thisLi[0]){
-									menuShowAry.splice(li,1);
-								}
-							}
-							//删除当前类比后，显示下一个Tag的内容
-							if($thisLi.next()){
-								$thisLi.next().find(" > a").trigger("click");
-							}else{
-								$imgBox_div.empty();
-							}
-							//清空页面上对应的内容
-							$thisLi.remove();
-						},
-						error:function(){
-							alert("抱歉，删除失败！");
-						}
-					});
-				}
-			});
-			$imgTagEditBtm.click(function(){
-				var $thisLi = $(this).parents("li"),
-					$imgTagText_a = $thisLi.find(" > a");
-					$imgTagText_div = $thisLi.find(" > div"),
-					imgTagId = $imgTagText_a.attr("tagId");
-				$imgTagText_a.hide();
-				$imgTagText_div.hide();
-				$thisLi.append('<input type="text" id="imgBox_h2_input" name="imgBox_h2_input" value="'+ $imgTagText_a.text() +'">');
-				$imgBox_h2_input = $("#imgBox_h2_input");
-				$imgBox_h2_input.focus().blur(function(){
-					imgTagText = $imgBox_h2_input.val();
-					
-					if( imgTagText != $imgTagText_a.text()){
-						$.ajax({
-							type:"post",
-							url:"updateImgTag.do",
-							data:{"imgTagId":imgTagId,"imgTagName":imgTagText},
-							dataType:"html/text",
-							cache:false,
-							beforeSend:function(){
-								//add waiting imgUrl
-							},
-							success:function(){
-								$imgTagText_a.find('span').html(imgTagText);
-								if($thisLi.hasClass("selected")){
-									$imgBox_h2_m.html(imgTagText);
-								}
-							},
-							error:function(){
-								alert("抱歉，更新失败！");
-							}
-						});
-					}
-					
-					$imgBox_h2_input.remove();
-					$imgTagText_a.show();
-					//$imgTagText_div.show();
-				});
-			});
-			
-			
-			
-			//script for detialText img
-			var $detialText = $(".detialText"),
-				$detialText_imgs = $detialText.find("img");
-			
-			$detialText_imgs.each(function(){
-				$(this).wrap("<div class='imgWrap'></div>");
-				var $imgWrap = $(this).parent();
-				$imgWrap.append("<img src='http://s.jesscool.com/imgpro/delete.gif' class='imgOperation' />");
-				$(".imgOperation").css({"left":$(this).width() + "px"});	
-			});
-			
-			
-			//script for removing img
-			$detialText.find('.imgWrap').each(function(){
-				var	$imgDelTip = $(this).find(".imgOperation"),
-					$thisImg = $(this).find("img").not(".imgOperation");
-				$imgDelTip.click(function(){
-					var imgId = $thisImg.attr('imgId');
-					var com_imgDel = confirm('真的要删除这张图片吗？');
-					if(com_imgDel){
-						$.ajax({
-							type:'post',
-							url:'deleteImg.do',
-							data:{'imgId':imgId},
-							dataType:"html/text",
-							cache:false,
-							success:function(){
-								$thisImg.parent().slideUp();
-							},
-							error:function(){
-								alert("抱歉，无法删除该图片！");
-							}
-						});
-					}
-				});
-			});
-			
-	});*/
+	
+	
 		
 })(jQuery);
--- jspro/profileManage.js
+++ jspro/profileManage.js
@@ -1,160 +1,127 @@
 
-(function(){
+(function($){
+	
+	var imgManager = {
+		changeImg: function(tagId){
+			$.ajax({
+				type:'post',
+				url:'changeImg.do',
+				data:{'tagId':tagId},
+				cache:false,
+				beforeSend:function(){
+					view.detailText.append( '<img class="loading" src="http://s.jesscool.com/imgpro/wait.gif" />' );
+				},
+				success: function(data){
+					view.renderImg(data);
+				},
+				error: function(){
+					alert( '抱歉，请求出错！' );
+				}
+			});
+		},
+		delImgTag: function(imgTagId){
+			$.ajax({
+				type:'post',
+				url:'deleteTag.do',
+				data:{"imgTagId":imgTagId},
+				cache:false,
+				success: function(data){
+					view.clickNextTag();
+				},
+				error: function(){
+					//alert( '抱歉，无法删除该图片！' );
+				}
+			});
+		},
+		updateTag: function(imgTagId,imgTagText,currentObj){
+			$.ajax({
+				type:'post',
+				url:'updateImgTag.do',
+				data:{"imgTagId":imgTagId,"imgTagName":imgTagText},
+				cache:false,
+				success: function(){
+					currentObj.html(imgTagText);
+				},
+				error: function(){
+					//alert( '抱歉，无法删除该图片！' );
+				}
+			});
+		}
+	}
 	
 	var view = {
 		init: function(){
 			this.getUIRef();
 			this.bindEvent();
+			this.renderInit();
 		},
 		getUIRef: function(){
-			this
+			this.imgNav1_ul = $( '#articleType' );
+			this.imgNav1_li = $( '#articleType > li' );
+			this.detailText = $( '.detailText' );
+			this.editAndDel = $('<div id="imgTagEditAndDel"><img src="http://s.jesscool.com/imgpro/delete.gif" class="imgTagDelBtm" alt="删除" /><img src="http://s.jesscool.com/imgpro/edit.gif" class="imgTagEditBtm" alt="编辑" /></div>');
+			this.imgTagDelBtm = this.editAndDel.find(".imgTagDelBtm");
+			this.imgTagEditBtm = this.editAndDel.find(".imgTagEditBtm");
 		},
 		bindEvent: function(){
-			
-		}
-
-			
-	}
-	
-	view.init();
-	
-	
-	/*$(function(){
-		//script for menu
-		var $articleType = $("#articleType"),
-			$articleType_li = $articleType.find("li"),
-			$editAndDel = $('<div id="imgTagEditAndDel"><img src="http://s.jesscool.com/imgpro/delete.gif" class="imgTagDelBtm" alt="删除" /><img src="http://s.jesscool.com/imgpro/edit.gif" class="imgTagEditBtm" alt="编辑" /></div>'),
-			$imgTagDelBtm = $editAndDel.find(".imgTagDelBtm"),
-			$imgTagEditBtm = $editAndDel.find(".imgTagEditBtm"),
-			time_editAndDel = 0;
-		$editAndDel.appendTo($("body"));
-		
-		$articleType_li.hover(function(){
-			//追加到当前对象
-			$editAndDel.appendTo($(this)).show().click(function(){ $(this).hide(); });
-		},function(){
-			$editAndDel.hide();
-		});
-		
-		
-		//删除、编辑功能
-			$imgTagDelBtm.click(function(){
-				var com_imgTagDel = confirm("真的要删除该分类吗?"),
-					$thisLi = $(this).parents("li");
-					imgTagId = $thisLi.find(" > a").attr("tagId");
-					
-				if(com_imgTagDel){
-					$editAndDel.appendTo($("body"));
-					$.ajax({
-						type:"post",
-						url:"deleteTag.do",
-						data:{"imgTagId":imgTagId},
-						dataType:"html/text",
-						cache:false,
-						beforeSend:function(){
-							//add waiting imgUrl
-						},
-						success:function(){
-							//清空数组中当前tag的内容
-							for(var li in menuShowAry){
-								if(menuShowAry[li] == $thisLi[0]){
-									menuShowAry.splice(li,1);
-								}
-							}
-							//删除当前类比后，显示下一个Tag的内容
-							if($thisLi.next()){
-								$thisLi.next().find(" > a").trigger("click");
-							}else{
-								$imgBox_div.empty();
-							}
-							//清空页面上对应的内容
-							$thisLi.remove();
-						},
-						error:function(){
-							alert("抱歉，删除失败！");
-						}
-					});
+			view.imgNav1_li.find( 'a' ).click(function(){
+				imgManager.changeImg($(this).attr( 'tagId' ));
+				$(this).addClass( 'selected' ).siblings().removeClass( 'selected' );
+			});
+			view.imgNav1_li.hover(function(){
+				//追加到当前对象
+				view.editAndDel.appendTo($(this)).show().click(function(){ $(this).hide(); });
+			},function(){
+				view.editAndDel.hide();
+			});
+			view.imgTagDelBtm.click(function(){
+				var sureToDel = confirm("真的要删除该分类吗?");
+				if(sureToDel){
+					imgManager.delImgTag( $(this).parents('li').find(' > a').attr('tagId') );
 				}
 			});
-			$imgTagEditBtm.click(function(){
+			view.imgTagEditBtm.click(function(){
 				var $thisLi = $(this).parents("li"),
-					$imgTagText_a = $thisLi.find(" > a");
-					$imgTagText_div = $thisLi.find(" > div"),
-					imgTagId = $imgTagText_a.attr("tagId");
+				$imgTagText_a = $thisLi.find(" > a");
+				$imgTagText_div = $thisLi.find(" > div"),
+				imgTagId = $imgTagText_a.attr("tagId");
 				$imgTagText_a.hide();
 				$imgTagText_div.hide();
 				$thisLi.append('<input type="text" id="imgBox_h2_input" name="imgBox_h2_input" value="'+ $imgTagText_a.text() +'">');
 				$imgBox_h2_input = $("#imgBox_h2_input");
 				$imgBox_h2_input.focus().blur(function(){
 					imgTagText = $imgBox_h2_input.val();
-					
 					if( imgTagText != $imgTagText_a.text()){
-						$.ajax({
-							type:"post",
-							url:"updateImgTag.do",
-							data:{"imgTagId":imgTagId,"imgTagName":imgTagText},
-							dataType:"html/text",
-							cache:false,
-							beforeSend:function(){
-								//add waiting imgUrl
-							},
-							success:function(){
-								$imgTagText_a.find('span').html(imgTagText);
-								if($thisLi.hasClass("selected")){
-									$imgBox_h2_m.html(imgTagText);
-								}
-							},
-							error:function(){
-								alert("抱歉，更新失败！");
-							}
-						});
+						imgManager.updateTag(imgTagId,imgTagText,$imgTagText_a);
 					}
-					
 					$imgBox_h2_input.remove();
 					$imgTagText_a.show();
-					//$imgTagText_div.show();
 				});
 			});
-			
-			
-			
-			//script for detialText img
-			var $detialText = $(".detialText"),
-				$detialText_imgs = $detialText.find("img");
-			
-			$detialText_imgs.each(function(){
-				$(this).wrap("<div class='imgWrap'></div>");
-				var $imgWrap = $(this).parent();
-				$imgWrap.append("<img src='http://s.jesscool.com/imgpro/delete.gif' class='imgOperation' />");
-				$(".imgOperation").css({"left":$(this).width() + "px"});	
-			});
-			
-			
-			//script for removing img
-			$detialText.find('.imgWrap').each(function(){
-				var	$imgDelTip = $(this).find(".imgOperation"),
-					$thisImg = $(this).find("img").not(".imgOperation");
-				$imgDelTip.click(function(){
-					var imgId = $thisImg.attr('imgId');
-					var com_imgDel = confirm('真的要删除这张图片吗？');
-					if(com_imgDel){
-						$.ajax({
-							type:'post',
-							url:'deleteImg.do',
-							data:{'imgId':imgId},
-							dataType:"html/text",
-							cache:false,
-							success:function(){
-								$thisImg.parent().slideUp();
-							},
-							error:function(){
-								alert("抱歉，无法删除该图片！");
-							}
-						});
-					}
-				});
-			});
-			
-	});*/
+		},
+		renderImg: function(data){
+			view.detailText.html(data);
+		},
+		clickNextTag: function(){
+			//删除当前类比后，显示下一个Tag的内容
+			if(view.imgNav1_li){
+				view.imgNav1_li.next().find(" > a").trigger("click");
+			}else{
+				view.detailText.empty();
+				view.imgNav1_ul.html( '<li>您还没有添加分类呢！</li>' );
+			}
+		},
+		renderInit: function(){
+			view.imgNav1_li.eq(0).addClass( 'selected' );
+		}
+	}
+	
+	$(function(){
+		view.init();
+	});
+	
+	
+	
+	
 		
 })(jQuery);
