b9b707230acbdd7594911ee3c8b7ace2f8ac6d6a
auscar <auscar@auscar-laptop.(none)>
2011-04-06 09:47:58 +0800

    首页新js

--- jspro/common.js
+++ jspro/common.js
@@ -1,6 +1,19 @@
-
 (function($){
+	var loader = {
+		curSet : [],
+		loadArtls : function(id){
+			var This = this;
+			$.ajax({
+				url : 'http://www.jesscool.com/moreLook.do?tagId='+id,
+				dataType : 'json',
+				success : function(obj){
+					This.curSet = obj.ary;		
+					view.renderArtlsList(This.curSet);
+				}
+			});		
+		}	
 	
+	};	
 	var view = {
 	    init : function(){
 	        this.getUIRef();
@@ -8,6 +21,8 @@
 	    },
 	    getUIRef : function(){
 	        this.articleType_li = $( '#articleType > li' );
+			this.box = $( 'div.articleBox', $('#main') );
+			console.log(this.box[ 0 ]);
 	    },
 	    bindEvent : function(){
 	    	//script for li of articleType
@@ -16,12 +31,42 @@
 	        },function(){
 	        	$(this).removeClass( 'hover' );
 	        });
-	    }
+	    },
+		renderArtlsList : function(ary){
+			var i = 0;
+			var html = '';
+			for( ; i<ary.length ;i++ ){
+				html += this.makeAArtls(ary[i]);	
+			}
+			this.box.html(html);
+		},
+		makeAArtls : function(art){
+			var t = art.intime.split('-');
+			var url = '/viewLook.do?lookId='+ art.id +'&tagId='+art.tagId;
+			return [
+				'<div class="article">',
+					'<h2 class="articleTitle">',
+						'<a target="_blank" href="'+ url +'">'+ art.title +'</a>',
+					'</h2>',
+					'<div class="articleDec">',
+						'<a target="_blank" href="'+ url +'">'+ art.content +'</a>',
+					'</div>',
+					'<div class="articleImg">',
+						'<a target="_blank" href="'+ url +'">'+ art.firstImg +'</a>',
+						'<dl class="articleDate">',
+							'<dt>'+ t[1]+'-'+t[2] +'</dt>',
+							'<dd>'+ t[0] +'</dd>',
+						'</dl>',
+					'</div>',
+				'</div>'
+			].join('');		
+		}
 	}
 	
 	// init
 	$(function(){
 		view.init();
+		loader.loadArtls(2);
 	});
 	
 	
